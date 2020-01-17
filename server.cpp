7ee4f88af7fd1731ea632c7d1a6a439e89f624b1
KamishiniNoYari <46847828+KamishiniNoYari@users.noreply.github.com>
2020-01-17 13:25:36 +0100

    Add changes and some debug

--- server.cpp
+++ server.cpp
@@ -8,11 +8,12 @@
 #include <mutex>
 #include "server.h"
 #include <unordered_map>
+#include <algorithm>
+
 
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wmissing-noreturn"
 using namespace std;
-
 
 //Global Variables
 int server_fd;
@@ -26,8 +27,11 @@ int main() {
     struct sockaddr_in new_address;
     int opt = 1;
     int addrlen = sizeof(new_address);
-    char buffer[BUFFER_SIZE];
+
+  //  char buffer[BUFFER_SIZE];
+
     pthread_t handlerThreadId;
+
     pthread_create(&handlerThreadId,NULL,timer_handler,(void *)&opt);
     //Create socket desc
     if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
@@ -37,7 +41,7 @@ int main() {
     }
 
     //Attach socket to the port
-    if(setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT,&opt,sizeof(opt)))
+    if(setsockopt(server_fd, SOL_SOCKET, SO_REUSEPORT,&opt,sizeof(opt)))
     {
         perror( " Setting socket opt failed");
         exit(EXIT_FAILURE);
@@ -97,37 +101,159 @@ int iscountry(string word,char letter)
     int result;
     int multiplies = 0;
     if(word[0]!=letter)result = 0;
-    else if ()
+    else{
+        int n = sizeof(countries)/sizeof(countries[0]);
+        auto itr = find(countries,countries+n,word);
+        if(itr!=end(countries))
+        {
+            for( const auto&[key, value]: global_answers){
+                if(value[0]==word)multiplies++;
+                if(multiplies>=2)
+                {
+                    result = 5;
+                    break;
+                }
+
+            }
+            if(multiplies<=1)result = 10;
+        }
+        else result = 0;
+    }
     return result;
 }
 int isanimal(string word,char letter)
 {
-    return 10;
+    int result;
+    int multiplies = 0;
+    if(word[0]!=letter)result = 0;
+    else{
+        int n = sizeof(animals)/sizeof(animals[0]);
+        auto itr = find(animals,animals+n,word);
+        if(itr!=end(animals))
+        {
+            for( const auto&[key, value]: global_answers){
+                if(value[2]==word)multiplies++;
+                if(multiplies>=2)
+                {
+                    result = 5;
+                    break;
+                }
+
+            }
+            if(multiplies<=1)result = 10;
+        }
+        else result = 0;
+    }
+    return result;
 }
 int iscity(string word, char letter)
 {
-    return 10;
+    int result;
+    int multiplies = 0;
+    if(word[0]!=letter)result = 0;
+    else{
+        int n = sizeof(cities)/sizeof(cities[0]);
+        auto itr = find(cities,cities+n,word);
+        if(itr!=end(cities))
+        {
+            for( const auto&[key, value]: global_answers){
+                if(value[1]==word)multiplies++;
+                if(multiplies>=2)
+                {
+                    result = 5;
+                    break;
+                }
+
+            }
+            if(multiplies<=1)result = 10;
+        }
+        else result = 0;
+    }
+    return result;
 }
 int isplant(string word, char letter)
 {
-    return 10;
+    int result;
+    int multiplies = 0;
+    if(word[0]!=letter)result = 0;
+    else{
+        int n = sizeof(plants)/sizeof(plants[0]);
+        auto itr = find(plants,plants+n,word);
+        if(itr!=end(plants))
+        {
+            for( const auto&[key, value]: global_answers){
+                if(value[3]==word)multiplies++;
+                if(multiplies>=2)
+                {
+                    result = 5;
+                    break;
+                }
+
+            }
+            if(multiplies<=1)result = 10;
+        }
+        else result = 0;
+    }
+    return result;
 }
 int isname(string word, char letter)
 {
+    int result;
+    int multiplies = 0;
+    if(word[0]!=letter)result = 0;
+    else{
+        int n = sizeof(names)/sizeof(names[0]);
+        auto itr = find(names,names+n,word);
+        if(itr != end(names))
+        {
+            for( const auto&[key, value]: global_answers){
+                if(value[4]==word)multiplies++;
+                if(multiplies>=2)
+                {
+                    result = 5;
+                    break;
+                }
+
+            }
+            if(multiplies<=1)result = 10;
+        }
+        else result = 0;
+    }
+    return result;
     return 10;
 }
+string placechecker(int fd,unordered_map<int,int> points)
+{
+    vector<pair<int,int>> elems(points.begin(),points.end());
+    sort(elems.begin(),elems.end(),[](const pair<int,int>& a, const pair<int,int>& b)
+    {
+        if(a.second != b.second) return a.second>b.second;
+        return a.first>b.first;
+    });
+    for(size_t i=0 ; i<elems.size();++i)
+    {
+        if(elems[i].first==fd)
+        {
+            return to_string(i+1);
+        }
+    }
 
+
+
+
+}
 void * timer_handler(void *arg)
 {
     char buffer[BUFFER_SIZE];
     bool gamestarted = false;
-    vector<string> emptyvector;
     srand(time(NULL));
+
     char random_letter;
     unordered_map<int, int> timeaswered;
     unordered_map<int, int> points;
     int actual_time = 0;
     int players_answered = 0;
+    int rounds = 0;
     while(true)
     {
         if (gamestart>=players/2 && players>=2) {
@@ -148,12 +274,13 @@ void * timer_handler(void *arg)
                     timeaswered[player] = -1;
                 }
                 //sendtoall("Wylosowana literka: "+random_letter);
+                rounds++;
 
                 while(actual_time<30 && players_answered <= players/2)
                 {
                     actual_time++;
                     for( const auto&[key, value]: global_answers){
-                        if (value == emptyvector){continue;}
+                        if (value.empty()){continue;}
                         else{if (timeaswered[key] ==-1) {
                                 timeaswered[key] = actual_time;
                                 players_answered++;
@@ -167,16 +294,20 @@ void * timer_handler(void *arg)
                 //calculation points
                 sendtoall("Calculating points\n");
                 for( const auto&[key, value]: global_answers){
-                    points[key]+=iscountry(value[0],random_letter)+iscity(value[1],random_letter)+
-                            isanimal(value[2],random_letter)+isplant(value[3],random_letter)+
+                    points[key]+=iscountry(value[0],random_letter)+
+                            iscity(value[1],random_letter)+
+                            isanimal(value[2],random_letter)+
+                            isplant(value[3],random_letter)+
                             isname(value[4],random_letter);
                     if (timeaswered[key] !=-1) {points[key]+=15 - 0.5*timeaswered[key];}
                     string point_mess=to_string(points[key]);
                     timeaswered[key]=-1;
-                    global_answers[key]=emptyvector;
+                    global_answers[key].clear();
                     char point_message[BUFFER_SIZE];
-                    memset(point_message,0,sizeof(point_mess));
-                    for(int i=0;i<sizeof(point_mess);i++){point_message[i]=point_mess[i];}
+                    memset(point_message,'\n',sizeof(point_mess));
+                    for(int i=0;i<sizeof(point_mess);i++){
+                        if(point_mess[i]=='\n')break;
+                        point_message[i]=point_mess[i];}
                     write(key,point_message,sizeof(point_message));
 
                 }
@@ -185,6 +316,20 @@ void * timer_handler(void *arg)
 
                 actual_time = 0;
                 for ( int player : sockets){timeaswered[player] = -1;}
+                if(rounds==roundsNumber)
+                {
+                    for (int player : sockets)
+                    {
+                        string place = "PLACE "+placechecker(player,points)+"\n";
+                        char cstr[place.size()+1];
+                        strcpy(cstr,place.c_str());
+                        write(player,cstr,place.size()+1);
+                        sendtoall("END\n");
+                    }
+                    //przechodzimy do wysyłania rankingu i odłączamy użytkowników
+                    //do zrobienia
+
+                }
             }
 
 
@@ -206,6 +351,11 @@ void * client_handler(void * arg)
         memset(buffer, 0, sizeof(buffer));
         valread = read( client_sock , buffer, BUFFER_SIZE);
         //write(client_sock,buffer,BUFFER_SIZE);
+        if(valread==-1)
+        {
+            perror("Read error");
+            exit(EXIT_FAILURE);
+        }
         string readstring = "";
         for(int i=0;i<=sizeof(buffer);i++){ if(buffer[i]!=NULL && buffer[i]!='\n')readstring.push_back(buffer[i]);}
 
@@ -228,6 +378,12 @@ void * client_handler(void * arg)
                 answerstring.push_back(readstring[i]);
             }
             global_answers[client_sock] = answers;
+
+        }
+        if(readstring.substr(0,3)==END)
+        {
+            close(client_sock);
+            pthread_exit(NULL);
 
         }
 
--- server.h
+++ server.h
@@ -27,6 +27,61 @@ using namespace std;
 //Commands
 const string START = "START";
 const string ANSWER = "AN";
+const string END = "END";
+const string cities[] = {"Augustów", "Ateny", "Antoninek", "Andrychów", "Anieliny", "Amsterdam","Będzin", "Barlinek", "Białystok",
+"Bielsko-Biała", "Cedynia", "Chełm", "Chorzów", "Chojnice", "Chrzanów", "Czeladź,Darłówko", "Dąbrowa Górnicza",
+"Dobrodzień", "Elbląg", "Ełk", "Edynburg", "Frombork", "Frampol", "Facja", "Fidżi","Gdynia", "Gdańsk", "Gniezno",
+"Gorzów Wielkopolski", "Halinów", "Hajnówka", "Hel", "Iława", "Imielin", "Inowrocław", "Jastrzębia Góra",
+"Jastrzębia Zdrój", "Kalisz", "Kalety", "Kazimierz Dolny","Legnica", "Legionowo", "Leśnica", "Leszno","Malbork",
+"Małogoszcz", "Marki", "Miastko", "Niepołomice", "Niemodlin", "Nidzica", "Olecko", "Olsztyn", "Opoczno", "Piaseczno",
+"Piekary Śląskie", "Pajęczno", "Pabianice", "Racibórz", "Rabka", "Raciąż", "Radom", "Sandomierz", "Siedlce", "Sanok",
+"Sosnowiec", "Tarchomin", "Tarczyn", "Tarnów", "Terespol", "Ujazd", "Ujście", "Ustroń", "Ustrzyki Dolne", "Wadowice",
+"Wałbrzych", "Warszawa", "Wieliczka,Zalewo", "Ząbki", "Zamość"};
+const string countries[] = {"Afganistan","Albania","Algieria","Andora","Angola","Antigua i Barbuda","Arabia Saudyjska","Argentyna",
+"Argentyna","Armenia","Australia","Austria","Azerbejdżan","Bahamy","Bahrajn","Bangladesz","Barbados","Belgia","Belize",
+"Benin","Bhutan","Białoruś","Boliwia","Bośnia i Hercegowina","Botswana","Brazylia","Brunei","Bułgaria","Burkina Faso",
+"Burundi","Chile","Chiny","Chorwacja","Cypr","Czad","Czarnogóra","Czechy","Dania","Demokratyczna Republika Konga",
+"Dominika","Dominikana","Dżibuti","Egipt","Ekwador","Erytrea","Estonia","Eswatini","Etiopia","Fidżi","Filipiny",
+"Finlandia","Francja","Gabon","Gambia","Ghana","Grecja","Grenada","Gruzja","Gujana","Gwatemala","Gwinea","Haiti",
+"Hiszpania","Holandia","Honduras","Indie","Indonezja","Irak","Iran","Irlandia","Islandia","Izrael","Jamajka","Japonia",
+"Jemen","Jordania","Kambodża","Kamerun","Kanada","Katar","Kazachstan","Kenia","Kirgistan","Kiribati","Kolumbia","Komory",
+"Kongo","Korea Południowa","Korea Północna","Kostaryka","Kuba","Kuwejt","Laos","Lesotho","Liban","Liberia","Libia",
+"Liechtenstein","Litwa","Luksemburg","Łotwa","Macedonia Północna","Madagaskar","Malawi","Malediwy","Malezja","Mali",
+"Malta","Maroko","Mauretania","Mauritius","Meksyk","Mikronezja","Mjanma","Mołdawia","Monako","Mongolia","Mozambik",
+"Namibia","Nauru","Nepal","Niemcy","Niger","Nigeria","Nikaragua","Norwegia","Nowa Zelandia","Oman","Pakistan","Palau",
+"Panama","Papua-Nowa Gwinea","Paragwaj","Peru","Polska","Południowa Afryka","Portugalia","Rosja","Rumunia","Rwanda",
+"Salwador","Samoa","San Marino","Senegal","Serbia","Seszele","Sierra Leone","Singapur","Słowacja","Słowenia","Somalia",
+"Sri Lanka","Stany Zjednoczone","Sudan","Surinam","Syria","Szwajcaria","Szwecja","Tadżykistan","Tajlandia","Tanzania",
+"Timor Wschodni","Togo","Tonga","Trynidad i Tobago","Tunezja","Turcja","Turkmenistan","Tuvalu","Uganda","Ukraina",
+"Urugwaj","Uzbekistan","Vanuatu","Watykan","Wenezuela","Węgry","Wielka Brytania","Wietnam","Włochy","Wybrzeże Kości Słoniowej",
+"Wyspy Marshalla","Wyspy Salomona","Zambia","Zimbabwe"};
+const string animals[] = {"ara", "antylopa", "albatros", "aligator", "anakonda", "alpaka", "arui", "biedronka", "bażant", "borsuk",
+"bóbr", "chomik", "czapla", "chrabąszcz", "cielak", "cap", "czajka", "delfin", "dinozaur", "daniel", "dzięcioł",
+"drozd", "dorsz", "dzik", "dudek", "emu", "empala", "eland", "elanus", "emberiza", "eremopila", "fenek", "foksterier",
+"fretka", "flądra", "flaming", "goryl", "gołąb", "gęś", "gawron", "halibut", "hiena", "hipopotam", "indyk", "iguana",
+"jaguar", "jastrząb", "jak", "jaskółka", "jamnik", "kot", "kogut", "koń", "kura", "lama", "lis", "lampart", "modliszka",
+"muł", "mamut", "mrówka", "norka", "niedźwiedź", "nietoperz", "orka", "osa", "okoń", "padalec", "paź", "pelikan",
+"rekin", "rak", "rybitwa", "słoń", "struś", "słowik", "tygrys", "tarantula", "tęcznik", "ukleja", "uszatek", "wilk",
+"wiewiórka", "wydra", "zając", "zebra", "zaskroniec"};
+const string plants[] = {"agawa", "aglonema", "akacja", "aloes", "ananas", "awokado", "arbuz", "agrest", "banan", "bób", "bakłażan",
+"burak", "bez", "borówki", "cytryna", "cebula", "chryzantema", "czereśnia", "czerwony grejfrut", "dąb", "dynia",
+"dracena", "daktyl", "dalia", "dąbrówka", "daskia", "eukaliptus", "echmea", "estragon", "fasola", "farbownik", "fołek",
+"floks", "fikus", "gazeta", "guma", "garnitur", "grzech", "genitalia", "hiacynt", "haworsja", "hiacynta", "iksja",
+"irga", "jarząb", "jałowiec", "jałówka", "kaczeniec", "kaktus", "kliwia", "klon", "klimek", "lilia", "lak", "lipa",
+"maciejka", "marzecianka", "mak", "magnolia", "niezapominajka", "nagietek", "nemezja", "narcyz", "ogórek", "orzech",
+"orlik", "papirus", "pelargonia", "pierwiosnek", "rannik", "rebucja", "rącznik", "rogownica", "róża", "sasanka",
+"surmia", "szałwia", "szczawik", "trawa", "tulipan", "tuja", "ubiorek", "ukwap", "werbana", "wiązówka", "widliczka",
+"wilec", "zawilec", "zimowit", "zielistka"};
+const string names[] = {"Ala", "Alicja", "Anna", "Adam", "Adolf", "Aleksander", "Adelajda", "Ambroży", "Barbara", "Beata", "Bartek",
+"Brandon" , "Błażej", "Borys", "Cyprian", "Cezary", "Daniel", "Dominik", "Dagmara", "Dobrusia", "Dalia", "Dawid",
+"Dominika", "Eliza", "Edmund", "Edyta", "Edward", "Ewa", "Filip", "Frania", "Funda", "Florencja", "Fido", "Fabian",
+"Feliks", "Gabriel", "Gabriela", "Hania", "Halina", "Henryk", "Iga", "Iza", "Justyna", "Karol", "Kamila", "Kamil",
+"Karolina", "Kinga", "Liza", "Laura", "Lena", "Lucyfer", "Leszek", "Małgosia", "Mateusz", "Marek", "Natalia",
+"Natan", "Nikola", "Ola", "Olek", "Paulina", "Paweł", "Patryk", "Piotrek", "Radek", "Róża", "Sebastian", "Staszek",
+"Tomek", "Tadeusz", "Ula", "Wiktoria", "Wiktor", "Zuzia", "Zdzisław"};
+const int roundsNumber = 5;
+
+
 void * client_handler(void * arg);
 void * timer_handler(void * arg);
 void sendtoall(string message);
@@ -35,7 +90,7 @@ int iscity(string word, char letter);
 int isplant(string word, char letter);
 int isanimal(string word, char letter);
 int isname(string word, char letter);
-
+string placechecker(int fd);
 
 
 #endif //SK_PROJ_SERVER_H
